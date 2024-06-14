9ea53c7c1ec95bf3cc531aaa1180c7dbb3b70294
HieuNghia6572 <phanhieunghia1054@gmail.com>
2024-06-14 15:40:28 +0700

    xong bai tuan 6

---
+++ TH_Java_F/src/main/java/com/example/TH_Java_F/entity/CustomUserDetail.java
@@ -0,0 +1,53 @@
+package com.example.TH_Java_F.entity;
+
+import com.example.TH_Java_F.repository.IUserRepository;
+import com.example.TH_Java_F.services.CustomUserDetailService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class CustomUserDetail implements UserDetails {
+    private final User user;
+    private final IUserRepository userRepository;
+    public CustomUserDetail(User user, IUserRepository userRepository){this.user = user;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
---
+++ TH_Java_F/src/main/java/com/example/TH_Java_F/services/UserService.java
@@ -0,0 +1,16 @@
+package com.example.TH_Java_F.services;
+
+import com.example.TH_Java_F.entity.User;
+import com.example.TH_Java_F.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private IUserRepository userRepository;
+
+    public void save(User user){
+        userRepository.save(user);
+    }
+}
